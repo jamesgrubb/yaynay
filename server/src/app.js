6c38ddcc292a9a6f123a78e363c509c2ab175386
jamesgrubb <jamesgrubb@mac.com>
2018-05-13 17:39:39 +0100

    setup of express js server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,23 @@
 console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+// parse application/json
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/satus', (req, res) => {
+    res.send({
+        message: 'Hello World!'
+    })
+})
+
+app.listen(process.env.PORT || 8081)
